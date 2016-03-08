5c0c02427f2998d39c07698072c9d7b5ba2a0f8a
gaurangsadekar <gaurang.sadekar@gmail.com>
2016-03-08 12:16:43 -0500

    multivariate linear regression model works like a charm, need to test some

--- hw2/get_stock_data.py
+++ hw2/get_stock_data.py
@@ -2,36 +2,33 @@
 use a base URL to get stock price data from google finance in a nice CSV format
 '''
 import urllib2
-
-BASE_URL = 'http://www.google.com/finance/getprices?'
-PARAMS = '&i=3600&p=1M&f=d,o,c,v' # gets opening & closing stock price
-                                         # every hour for 1 month
+# https://www.google.com/finance/historical?q=NYSE%3AMCD&output=csv
+BASE_URL = 'http://www.google.com/finance/historical?'
+PARAMS = '&output=csv'  # this URL gets historical data for 1 year by default
 stocks = [('BAC', 'NYSE'),('C', 'NYSE'), ('IBM', 'NYSE'), ('AAPL', 'NASDAQ'), ('GE', 'NYSE'), ('T', 'NYSE')]
 nyse_stocks = ['BAC', 'C', 'IBM', 'GE', 'T', 'MCD', 'NKE', 'TWTR']
 nasdaq_stocks = ['AAPL', 'TSLA']
 
 for stock in nyse_stocks:
-    # q=AAPL&x=NASDAQ
-    company_str = 'q=' + stock
-    exchange = '&x=' + 'NYSE'
-    target_url = BASE_URL + company_str + exchange + PARAMS
+    nyse_query = 'q=NYSE%3A'
+    company_str = nyse_query + stock
+    target_url = BASE_URL + company_str + PARAMS
     data = urllib2.urlopen(target_url)
-    filename = 'stock_hourly_data/' + stock + '.txt'
+    filename = 'stock_hourly_data/' + stock + '.csv'
     f = open(filename, 'a')
     for line in data:
         f.write(line)
-    print 'got data for ', stock
+    print 'got data for', stock
     f.close()
 
 for stock in nasdaq_stocks:
-    # q=AAPL&x=NASDAQ
-    company_str = 'q=' + stock
-    exchange = '&x=' + 'NASD'
-    target_url = BASE_URL + company_str + exchange + PARAMS
+    nasdaq_query = 'q=NASDAQ%3A'
+    company_str = nasdaq_query + stock
+    target_url = BASE_URL + company_str + PARAMS
     data = urllib2.urlopen(target_url)
-    filename = 'stock_hourly_data/' + stock + '.txt'
+    filename = 'stock_hourly_data/' + stock + '.csv'
     f = open(filename, 'a')
     for line in data:
         f.write(line)
-    print 'got data for ', stock
+    print 'got data for', stock
     f.close()
